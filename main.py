b73c01fdb6dc15cc30277243efb7fe10d1cca3b9
NeryBarrientos201807086 <3386309102008@ingenieria.usac.edu.gt>
2020-08-24 23:05:07 -0600

    cargar varios archivos

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import json
 
 def cargar_datos(ruta):
-    with open(ruta) as archivos:
-        datos = json.loads(archivos.read())
-    for element in datos:
-        print(element)
+    print(ruta)
+    for i in ruta:
+        with open(i + ".json") as archivos:
+            datos = json.loads(archivos.read())
+            print(datos)
 
-#print("Escriba el nombre del archivo a cargar")
-#ruta = input()
-#cargar_datos(ruta)
 
 while True:
     print('Escoja una opcion: \n CARGAR \n SELECCIONAR \n MAXIMO \n MINIMO \n SUMA \n CUENTA \n REPORTAR \n 8.SALIR')
@@ -16,8 +14,7 @@ while True:
     if opt == 'CARGAR':
         print("Escriba el nombre del archivo a cargar")
         print()
-        ruta = input()
-        ruta = ruta + '.json'
+        ruta = input().split(',')
         cargar_datos(ruta)
     elif opt == 'SELECCIONAR':
         print("opcion 2")
