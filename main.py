9507714af565e53e5791bd901c07ba9b4f0415c9
NeryBarrientos201807086 <3386309102008@ingenieria.usac.edu.gt>
2020-08-26 19:48:31 -0600

    opcion cargar correcta

--- main.py
+++ main.py
@@ -1,35 +1,50 @@
 import json
-
+import re
+listacargada = []
+ruta = []
 def cargar_datos(ruta):
-    print(ruta)
+    #print(ruta)
     for i in ruta:
+        contador = 0
         with open(i + ".json") as archivos:
             datos = json.loads(archivos.read())
-            dato=[]
-            dato.append(datos)
-            print(dato)
+            listacargada.append(datos)
+            print("Archivo: " + i + " cargado")
 
-
-while True:
-    print('Escoja una opcion: \n CARGAR \n SELECCIONAR \n MAXIMO \n MINIMO \n SUMA \n CUENTA \n REPORTAR \n 8.SALIR')
-    opt = input()
-    if opt == 'CARGAR':
-        print("Escriba el nombre del archivo a cargar")
-        print()
-        ruta = input().split(',')
+def main():
+    print("estas en el menu")
+    opcion = input().split(" ")
+    ruta = opcion[::]
+    ruta.pop(0)
+    #print(ruta)
+    if (opcion[0].lower() == 'cargar'):
         cargar_datos(ruta)
-    elif opt == 'SELECCIONAR':
-        print("opcion 2")
-    elif opt == 'MAXIMO':
+        print(listacargada[0][0]['nombre'])
+        #print(listacargada)
+        main()
+    elif opt == '2':
+        print("hola")
+        print(listacargada)
+    elif opt == 'maximo':
         print("opcion 3")
-    elif opt == 'MINIMO':
+    elif opt == 'minimo':
         print("opcion 4")
-    elif opt == 'SUMA':
+    elif opt == 'suma':
         print("opcion 5")
-    elif opt == 'CUENTA':
+    elif opt == 'cuenta':
         print("opcion 6")
-    elif opt == 'REPORTAR':
+    elif opt == 'reportar':
         print("opcion 7")
     else:
         exit()
         print()
+
+def opcion_cargar(opt):
+    if (opt == 'cargar'):
+        print("Escriba el nombre del archivo a cargar")
+        print()
+        ruta = input().split(',')
+        listacargada.append(cargar_datos(ruta))
+        print(listacargada)
+
+main()
