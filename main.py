5b420acc86a49a8a6bc07f17c481288a679fcc35
NeryBarrientos201807086 <3386309102008@ingenieria.usac.edu.gt>
2020-09-01 14:05:08 -0600

    selecionar  *, minimo y max

--- main.py
+++ main.py
@@ -1,39 +1,82 @@
 import json
-import re
+import numpy as np
 listacargada = []
 ruta = []
+opcion = []
+maximo = listacargada
+max_edad = []
+max_promedio = []
+n = "Nombre : "
+e = "Edad : "
+a = "Activo : "
+p = "Promedio : "
+atributos = []
 def cargar_datos(ruta):
     #print(ruta)
     for i in ruta:
-        contador = 0
         with open(i + ".json") as archivos:
             datos = json.loads(archivos.read())
             listacargada.append(datos)
             print("Archivo: " + i + " cargado")
+    contador = 0
+    for i in listacargada:
+        for j in range(len(i)):
+            max_edad.append(listacargada[contador][j]['edad'])
+            max_promedio.append(listacargada[contador][j]['promedio'])
+        contador += 1
 
 def main():
     print("estas en el menu")
-    opcion = input().split(" ")
-    ruta = opcion[::]
-    ruta.pop(0)
-    #print(ruta)
-    if (opcion[0].lower() == 'cargar'):
+    opcion = input().split(",")
+    ruta = opcion
+    atributos = opcion
+    comando = opcion[0][:6]
+    comando1 = opcion[0][:11]
+    todos = opcion[0][12:13]
+    ruta[0] = ruta[0].replace(" ","")
+    ruta[0] = ruta[0][6:]
+    if (comando.lower() == 'cargar'):
+        for i in range(len(ruta)):
+            ruta[i] = ruta[i].replace(" ", "")
         cargar_datos(ruta)
-        print(listacargada[0][0]['nombre'])
-        #print(listacargada)
-        main()
-    elif opt == '2':
-        print("hola")
         print(listacargada)
-    elif opt == 'maximo':
-        print("opcion 3")
-    elif opt == 'minimo':
-        print("opcion 4")
-    elif opt == 'suma':
+        main()
+    elif (comando1.lower() == 'seleccionar'):
+        #print("Que atributos desea buscar")
+        #atributos = input().split(',')
+        #print(atributos)
+        if (todos == '*'):    
+            imprimir_todos()
+            main()
+        else:
+            main()
+    elif (comando.lower() == 'maximo'):
+        for i in range(len(atributos)):
+            atributos[i] = atributos[i].replace(" ", "")
+            #print(atributos)
+            if atributos[0] == 'edad':
+                edadmaxima = np.array(max_edad)
+                print("La edad maxima es : " + str(np.amax(edadmaxima)))
+            elif atributos[0] == 'promedio':
+                promediomaximo = np.array(max_promedio)
+                print("El promedio maximo es : " + str(np.amax(promediomaximo)))
+        main()
+    elif (comando.lower() == 'minimo'):
+        for i in range(len(atributos)):
+            atributos[i] = atributos[i].replace(" ", "")
+            #print(atributos)
+            if atributos[0] == 'edad':
+                edadminima = np.array(max_edad)
+                print("La edad minima es : " + str(np.amin(edadminima)))
+            elif atributos[0] == 'promedio':
+                promediominimo = np.array(max_promedio)
+                print("El promedio minimo es : " + str(np.amin(promediominimo)))
+        main()
+    elif (comando.lower() == 'suma'):
         print("opcion 5")
-    elif opt == 'cuenta':
+    elif (comando.lower() == 'cuenta'):
         print("opcion 6")
-    elif opt == 'reportar':
+    elif (comando.lower() == 'reportar'):
         print("opcion 7")
     else:
         exit()
@@ -46,5 +89,18 @@ def opcion_cargar(opt):
         ruta = input().split(',')
         listacargada.append(cargar_datos(ruta))
         print(listacargada)
+
+def imprimir_todos():
+    print(listacargada)
+    contador = 0
+    for i in listacargada:
+        for j in range(len(i)):
+            print("--------------------------")
+            print('{}{}'.format(n,listacargada[contador][j]['nombre']))
+            print('{}{}'.format(e,listacargada[contador][j]['edad']))
+            print('{}{}'.format(a,listacargada[contador][j]['activo']))
+            print('{}{}'.format(p,listacargada[contador][j]['promedio']))
+            print("--------------------------")
+        contador += 1
 
 main()
